d2d05337db2e152f048f2c1892daeb668b698155
cherrycyanide <isobellwaite@student.cascadia.edu>
2026-01-22 15:05:13 -0800

    Created Error Message for invalid lucky number

--- LuckySpin/Models/Player.cs
+++ LuckySpin/Models/Player.cs
@@ -4,8 +4,11 @@ namespace LuckySpin.Models
     public class Player 
     {
         //TODO: Annotate both Player properties as shown in the exercise Figure 1 and the resources section.
+        [Required(ErrorMessage = "First Name Is required")]
         public string FirstName { get; set; } = "Player";
 
-        public int Luck { get; set; } = 7;
+        [Required(ErrorMessage = "Lucky Number is required")]
+        [Range(1,9, ErrorMessage = "Lucky Number is required")]
+        public int Luck { get; set; }
     }
 }
